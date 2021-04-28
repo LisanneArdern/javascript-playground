8fbf4553f1bcae2d3fe779be4ffe40f03e677727
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-04-28 14:38:04 +0200

    Add toUpperCase

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const form = document.querySelector('[data-form]')
-const input = document.querySelector('[name=class]')
 const box = document.querySelector('[data-box]')
+const input = document.querySelector('[name=class]')
+const output = document.querySelector('[data-output]')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
-  // box.classList.toggle(input.value)
   box.className = 'box ' + input.value
+  // output.innerText = input.value.toUpperCase()
+  // input.value = ''
+  // input.focus()
+
+  const inputText = input.value
   input.value = ''
   input.focus()
+  const outputText = inputText.toUpperCase()
+  output.value = outputText
 })
